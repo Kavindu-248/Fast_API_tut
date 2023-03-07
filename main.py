2b2928d5f46ab204f08eab3a0735e14aca03a2ae
Kavindu Fernando <kavindu2331@gmail.com>
2023-03-07 15:41:30 +0530

    feat: added the database list

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI
+from models import User, Gender, Role
+from typing import List
+from uuid import uuid4
 
 
 app = FastAPI()
 
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="John",
+        last_name="Doe",
+        gender=Gender.male,
+        roles=[Role.student]
+    ),
+
+    User(
+        id=uuid4(),
+        first_name="Jane",
+        last_name="Jackson",
+        gender=Gender.female,
+        roles=[Role.admin, Role.user]
+    ),
+
+
+
+]
+
 
 @app.get("/")
-def read_root():
+async def root():
     return {"Hello": "World"}
