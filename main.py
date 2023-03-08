94050e23a38e66bc89fe2ce066ba9194e4d06b49
Kavindu Fernando <kavindu2331@gmail.com>
2023-03-08 09:03:31 +0530

    feat: added delete request

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from models import User, Gender, Role
 from typing import List
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 
 app = FastAPI()
@@ -40,3 +40,11 @@ async def fetch_users():
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
